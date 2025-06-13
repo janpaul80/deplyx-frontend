fb55fee6c7655a681ada357990c5d61460ae0f35
JP Hartmann <jp@janpaul.be>
2025-06-12 20:02:41 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        mono: ['Roboto Mono', 'monospace']
+      }
+    },
+  },
+  plugins: [],
+}
